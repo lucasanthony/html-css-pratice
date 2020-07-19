bd8e101bf8c7dbf0169f9609c89169b9ad46e9b0
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2020-07-19 14:29:47 -0300

    social media

---
+++ social-media/index.js
@@ -0,0 +1,10 @@
+function openSite(site) {
+    if (site == 'linkedin')
+        window.open('http://linkedin.com/in/lucasanthony40')
+
+    if (site == 'instagram')
+        window.open('http://instagram.com/lucasanthony40')
+
+    if (site == 'github')
+        window.open('http://github.com/lucasanthony')
+}
